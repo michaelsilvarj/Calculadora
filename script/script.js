360d855028cbe0a02a5e425d2cb57fddcb56fdbc
Michael Silva <michaelsilv@gmail.com>
2022-03-29 22:31:14 -0300

    Alteração do arquivo index nas classes de botões e adição das funções playaudio e soundonoff para a adição de som ao botão

--- script/script.js
+++ script/script.js
@@ -1,20 +1,38 @@
 
 function calcular(tipo, valor){
     if(tipo === 'acao') {
-      if(valor === 'c'){
         //limpar o visor
+      if(valor === 'c'){        
         document.getElementById('resultado').value = '';
       }
+    }
       if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
         document.getElementById('resultado').value += valor;
       }
       if(valor === '=') {
         var valor_campo = eval(document.getElementById('resultado').value);
         document.getElementById('resultado').value = valor_campo;
-      }
-    } else if (tipo === 'valor') {
+
+      }else if (tipo === 'valor') {
 
       document.getElementById('resultado').value += valor;
     }
-  }
-   // 
+}
+
+
+// função ativar e desativar audio
+let audio = false;
+function playaudio (botao){
+    if (audio === false) {
+        audio = true;
+        console.log('audio ativado');
+
+    } else if (audio === true) {
+        audio = false;
+        console.log('audio desativado');
+    }
+}
+   
+soundonoff(){
+    
+}
